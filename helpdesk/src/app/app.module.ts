bbab9db6a16bf04983eef8c40c927e95ead22f8b
Adriano-Cardoso <goularta97@gmail.com>
2023-06-18 10:55:54 -0300

    Configurando rota de login

--- helpdesk/src/app/app.module.ts
+++ helpdesk/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { NavComponent } from './components/nav/nav.component';
 
 // Componentes do projeto
 /*import { NavComponent } from './components/nav/nav.component';
@@ -52,7 +53,8 @@ import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
